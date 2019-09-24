86f81cc39cd8a26bed7ace8bce969fe8121203d8
Vyshnavi Mohan <Vyshnavi Mohan@VYSHAKH>
2019-09-24 14:53:11 +0530

    intial commit by Vyshnavi

---
+++ SalesToolversion2019/src/com/faith/test/Main.java
@@ -0,0 +1,12 @@
+package com.faith.test;
+
+public class Main {
+	
+	public static void main(String[]args) {
+		
+		SalesData objSalesData=new SalesData();
+		System.out.println("Welcome to Sales App");
+		
+	}
+	
+}
---
+++ SalesToolversion2019/src/com/faith/test/SalesData.java
@@ -0,0 +1,5 @@
+package com.faith.test;
+
+public class SalesData {
+
+}
