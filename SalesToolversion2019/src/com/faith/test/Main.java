3b3d1658163d1e7fa1514d50f2dfebbdf51c7291
dev30 <dev30@dev30-G41T-M7>
2019-09-24 17:29:57 +0530

    arsha commit

--- SalesToolversion2019/src/com/faith/test/Main.java
+++ SalesToolversion2019/src/com/faith/test/Main.java
@@ -6,7 +6,7 @@ public class Main {
 		
 		SalesData objSalesData=new SalesData();
 		System.out.println("Welcome to Sales App");
-		System.out.println("Testtttt");
+		System.out.println("Testtttt......a..");
 		
 	}
 	
